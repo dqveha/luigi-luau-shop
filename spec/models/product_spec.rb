0f0892a567009d55412829e244ec30965fc8e560
Dave Lindqvist <Lindqvist.Dave@gmail.com>
2021-08-06 09:11:23 -0700

    Pass tests for validating presence of product properties

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -4,4 +4,9 @@ require 'pry'
 describe Product do
   it { should have_many(:reviews) }
 
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:cost) }
+  it { should validate_presence_of(:country_of_origin) }
+
+
 end
