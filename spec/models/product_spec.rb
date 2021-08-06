017ddc40ddb6543e02073914bd3bf9de211278fb
Dave Lindqvist <Lindqvist.Dave@gmail.com>
2021-08-06 09:15:15 -0700

    Pass test for titleizing product name and country of origin after creation

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -8,5 +8,7 @@ describe Product do
   it { should validate_presence_of(:cost) }
   it { should validate_presence_of(:country_of_origin) }
 
-
+  it("titleizes the name of the product and country of origin") do
+    product = Product.create({name: "chef's knife", cost: 24.99, country_of_origin: "thailand"})
+  end
 end
