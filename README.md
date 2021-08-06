53ff20a47a067508775b6c94b79598a10c9868ae
Dave Lindqvist <Lindqvist.Dave@gmail.com>
2021-08-06 16:42:08 -0700

    Update README file

--- README.md
+++ README.md
@@ -1,8 +1,82 @@
-#
+# Luigi's Lūʻau Specialty Shop
 
+## [https://github.com/dqveha/luigi-luau-shop](https://github.com/dqveha/luigi-luau-shop)
 
-<!-- Bugs/Work in progress
+#### By Dave Lindqvist
 
-1. Unable to get before_save to work with Review class; tested on record store practice, and does not work as well for some reason.
-2. Decimals do not go to the hundreths place
-3. Will create separate pages, if desired, to feature three most recently added products and products with the most reviews. -->
+---
+
+##
+
+This is the tenth independent project (Ruby, Week 4) assigned by Epicodus that will be broken up into two weeks. This first week is assessing the implementation of Ruby on Rails, creating a database to include a one-to-many relationship between 'Products' and 'Reviews.' The main emphases are incorporating scopes, validations, callbacks, seeding, and flash messages to the project.
+
+For a visual relationship representation of the database:
+
+## ![luigi_specialty_database](https://github.com/dqveha/luigi-luau-shop/blob/main/database-visual.png?raw=true)
+
+## Technologies Used
+
+1. Docker
+2. Ruby + RubyGems
+3. RSpec & Capybara
+4. Rails
+5. PostgreSQL
+
+## Installation Instructions
+
+1. Open your terminal
+2. Clone this repo to your desktop:
+
+```
+https://github.com/dqveha/luigi-luau-shop.git
+```
+
+3. Open the `luigi-luau-shop` directory using Visual Studio Code
+4. Run command `docker-compose up --build` in your terminal
+5. Find out your container ID within docker by typing in the terminal:
+
+```
+docker ps
+```
+
+6. (a) Copy the container ID related to the database and load the database_backup.sql within the folder by typing in:
+
+```
+docker exec [container_ID] pg_dump -U postgres luigi_specialty_development < database_backup.sql
+```
+
+    + (b) If step 6(a) does not work, replace 'luigi_specialty_development' with 'postgres' and try again.
+    + (c) If step 6(a) and 6(b) does not work through Docker, follow these steps here within the terminal to create the database yourself:
+
+    docker exec -it -u postgres [container_ID] psql
+    CREATE DATABASE luigi_specialty_development;
+    \c luigi_specialty_development
+    CREATE TABLE projects (title varchar, id serial PRIMARY KEY);
+    CREATE TABLE volunteers (name varchar, project_id int, id serial PRIMARY KEY);
+
+7. The user can open the webpage by visiting this URL in the browser:
+
+```
+http://localhost:3000/
+```
+
+8. To run RSpec and see if the tests are passing, type into the terminal:
+
+```
+docker-compose run web bundle exec rspec
+```
+
+## Bugs:
+
+1. Unable to get before_save to work with Review class properly
+2. Decimals do not list to the hundreths place if product's listing price is 1.1, 1.2, 1.3, etc.
+
+## Work in progress:
+
+1. Will create separate pages, if desired, to feature three most recently added products and products with the most reviews
+2. Creating a drop-down menu for the country of origin when adding a product
+3. Improving overall layout using Bootstrap and CSS
+
+---
+
+### License is GPLv3 and I make no claim to copyright.
---
+++ database_backup.sql
@@ -0,0 +1,231 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 12.1 (Debian 12.1-1.pgdg100+1)
+-- Dumped by pg_dump version 12.1 (Debian 12.1-1.pgdg100+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: ar_internal_metadata; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.ar_internal_metadata (
+    key character varying NOT NULL,
+    value character varying,
+    created_at timestamp without time zone NOT NULL,
+    updated_at timestamp without time zone NOT NULL
+);
+
+
+ALTER TABLE public.ar_internal_metadata OWNER TO postgres;
+
+--
+-- Name: products; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.products (
+    id bigint NOT NULL,
+    name character varying,
+    cost double precision,
+    country_of_origin character varying,
+    created_at timestamp without time zone NOT NULL,
+    updated_at timestamp without time zone NOT NULL
+);
+
+
+ALTER TABLE public.products OWNER TO postgres;
+
+--
+-- Name: products_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.products_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.products_id_seq OWNER TO postgres;
+
+--
+-- Name: products_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.products_id_seq OWNED BY public.products.id;
+
+
+--
+-- Name: reviews; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.reviews (
+    id bigint NOT NULL,
+    author character varying,
+    content_body text,
+    rating integer,
+    product_id integer,
+    created_at timestamp without time zone NOT NULL,
+    updated_at timestamp without time zone NOT NULL
+);
+
+
+ALTER TABLE public.reviews OWNER TO postgres;
+
+--
+-- Name: reviews_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.reviews_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.reviews_id_seq OWNER TO postgres;
+
+--
+-- Name: reviews_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.reviews_id_seq OWNED BY public.reviews.id;
+
+
+--
+-- Name: schema_migrations; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.schema_migrations (
+    version character varying NOT NULL
+);
+
+
+ALTER TABLE public.schema_migrations OWNER TO postgres;
+
+--
+-- Name: products id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.products ALTER COLUMN id SET DEFAULT nextval('public.products_id_seq'::regclass);
+
+
+--
+-- Name: reviews id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.reviews ALTER COLUMN id SET DEFAULT nextval('public.reviews_id_seq'::regclass);
+
+
+--
+-- Data for Name: ar_internal_metadata; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.ar_internal_metadata (key, value, created_at, updated_at) FROM stdin;
+environment	development	2021-08-06 15:29:01.773674	2021-08-06 15:29:01.773674
+\.
+
+
+--
+-- Data for Name: products; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.products (id, name, cost, country_of_origin, created_at, updated_at) FROM stdin;
+509	Asdfsaas	12.12	U S	2021-08-06 23:27:25.371232	2021-08-06 23:27:25.371232
+\.
+
+
+--
+-- Data for Name: reviews; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.reviews (id, author, content_body, rating, product_id, created_at, updated_at) FROM stdin;
+\.
+
+
+--
+-- Data for Name: schema_migrations; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.schema_migrations (version) FROM stdin;
+20210806152330
+20210806152950
+20210806153414
+\.
+
+
+--
+-- Name: products_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.products_id_seq', 509, true);
+
+
+--
+-- Name: reviews_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.reviews_id_seq', 73858, true);
+
+
+--
+-- Name: ar_internal_metadata ar_internal_metadata_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.ar_internal_metadata
+    ADD CONSTRAINT ar_internal_metadata_pkey PRIMARY KEY (key);
+
+
+--
+-- Name: products products_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.products
+    ADD CONSTRAINT products_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: reviews reviews_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.reviews
+    ADD CONSTRAINT reviews_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: schema_migrations schema_migrations_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.schema_migrations
+    ADD CONSTRAINT schema_migrations_pkey PRIMARY KEY (version);
+
+
+--
+-- Name: reviews fk_rails_bedd9094d4; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.reviews
+    ADD CONSTRAINT fk_rails_bedd9094d4 FOREIGN KEY (product_id) REFERENCES public.products(id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
