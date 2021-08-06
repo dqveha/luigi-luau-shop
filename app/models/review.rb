9aa46dfd4a09ae414cf46b4a6cfc2dc256693b1e
Dave Lindqvist <Lindqvist.Dave@gmail.com>
2021-08-06 09:43:56 -0700

    Pass tests for validating presence of review properties

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,9 @@
 class Review < ApplicationRecord
   belongs_to :product
+
+  validates :author, presence: true
+  validates :content_body, presence: true
+  validates :rating, presence: true
+  validates :product_id, presence: true
+
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -3,4 +3,11 @@ require 'pry'
 
 describe Review do
   it { should belong_to(:product) }
+
+  it { should validate_presence_of(:author) }
+  it { should validate_presence_of(:content_body) }
+  it { should validate_presence_of(:rating) }
+  it { should validate_presence_of(:product_id) }
+
+  
 end
