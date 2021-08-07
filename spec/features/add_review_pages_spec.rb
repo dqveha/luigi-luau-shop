9ee175458aaf80a7ac92e02ae769179576acfc8d
Dave Lindqvist <Lindqvist.Dave@gmail.com>
2021-08-06 18:06:56 -0700

    Test integration spec with adding a review to a product; test pass

--- spec/features/add_review_pages_spec.rb
+++ spec/features/add_review_pages_spec.rb
@@ -1,22 +1,24 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# describe "the add a review process" do
-#   it "adds a new review" do
-#     test_product = Product.create({:name => "Chef's Knife", :cost => "24.99", :country_of_origin => "Thailand"})
-#     visit products_path
-#     within('.find_me') do
-#       click_on ("Chef\'s Knife")
-#     end
-#     click_on ("Add a review")
-#     fill_in "Author", :with =>
-#     fill_in "Rating", :with =>
-#     fill_in "Review", :with =>
-#   end
+describe "the add a review process" do
+  it "adds a new review" do
+    test_product = Product.create({:name => "Chef's Knife", :cost => "24.99", :country_of_origin => "Thailand"})
+    visit products_path
+    within('.find_me') do
+      click_on ("Chef\'s Knife")
+    end
+    click_on ("Add a review")
+    fill_in "Author", :with => "Dave"
+    fill_in "Rating", :with => "5"
+    fill_in "review[content_body]", :with => "It cut through a mountain, like the same way you've seen in a usual anime trope."
+    click_on ("Create Review")
+    expect(page).to have_content "Dave"
+  end
 
-#   it "gives an error when no name is entered" do
-#     visit new_product_path
-#     click_on 'Create Product'
-#     expect(page).to have_content "Name can't be blank"
-#   end
+  # it "gives an error when no name is entered" do
+  #   visit new_product_path
+  #   click_on 'Create Product'
+  #   expect(page).to have_content "Name can't be blank"
+  # end
 
-# end
+end
