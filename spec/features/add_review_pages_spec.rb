1e2629b2718b90586fc167475c644b236df3c3ed
Dave Lindqvist <Lindqvist.Dave@gmail.com>
2021-08-06 18:12:07 -0700

    Test integration spec where entry is blank when leaving a review; test pass

--- spec/features/add_review_pages_spec.rb
+++ spec/features/add_review_pages_spec.rb
@@ -10,15 +10,20 @@ describe "the add a review process" do
     click_on ("Add a review")
     fill_in "Author", :with => "Dave"
     fill_in "Rating", :with => "5"
-    fill_in "review[content_body]", :with => "It cut through a mountain, like the same way you've seen in a usual anime trope."
+    fill_in "review[content_body]", :with => "It cuts through a mountain the same way you've seen in a usual anime trope."
     click_on ("Create Review")
     expect(page).to have_content "Dave"
   end
 
-  # it "gives an error when no name is entered" do
-  #   visit new_product_path
-  #   click_on 'Create Product'
-  #   expect(page).to have_content "Name can't be blank"
-  # end
+  it "gives an error when no author is entered" do
+    test_product = Product.create({:name => "Chef's Knife", :cost => "24.99", :country_of_origin => "Thailand"})
+    visit products_path
+    within('.find_me') do
+      click_on ("Chef\'s Knife")
+    end
+    click_on ("Add a review")
+    click_on ("Create Review")
+    expect(page).to have_content "There was a problem uploading your review. Please provide a review with a rating between 1 to 5, and entry of 50 to 250 characters"
+  end
 
 end
