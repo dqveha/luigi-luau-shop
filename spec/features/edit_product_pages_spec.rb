2f372f491d7a2164ab8deb224934a097e8722040
Dave Lindqvist <Lindqvist.Dave@gmail.com>
2021-08-06 17:54:18 -0700

    Test integration spec for updating a product; test pass

---
+++ spec/features/edit_product_pages_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe "the edit a product process" do
+  it "edits a product" do
+    test_product = Product.create({:name => "Chef's Knife", :cost => "24.99", :country_of_origin => "Thailand"})
+    visit products_path
+    within('.find_me') do
+      click_on ("Chef\'s Knife")
+    end
+    click_on 'Edit'
+    fill_in "Name", :with => "Chef's Spoon"
+    fill_in "Cost", :with => "79.99"
+    fill_in "product[country_of_origin]", :with => "Bowser's Castle"
+    click_on 'Update Product'
+    expect(page).to have_content "Chef\'s Spoon"
+  end
+
+end
