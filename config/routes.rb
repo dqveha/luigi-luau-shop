4272f90b27d706c85893cd997c5c91da8ce06b36
Dave Lindqvist <Lindqvist.Dave@gmail.com>
2021-08-13 08:24:05 -0700

    Create user controller and model

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,18 @@ Rails.application.routes.draw do
 
   get '/index' => 'index#index'
 
+  # get '/signup' => 'users#new'
+  # post '/users' => 'users#create'
+
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'sessions#create'
+  get '/signout' => 'sessions#destroy'
+
   get '/products/most' => 'products#most'
   get '/products/recent' => 'products#recent'
   get '/products/us' => 'products#us'
   
+  resources :users
   resources :products do
     resources :reviews
   end
