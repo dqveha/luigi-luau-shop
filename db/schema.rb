dc4d2e124396c0e4e2fc61b06f186196ea2f72be
Dave Lindqvist <Lindqvist.Dave@gmail.com>
2021-08-14 13:44:51 -0700

    Add featured column to products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_13_150508) do
+ActiveRecord::Schema.define(version: 2021_08_14_204225) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2021_08_13_150508) do
     t.string "country_of_origin"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "featured", default: false
   end
 
   create_table "reviews", force: :cascade do |t|
