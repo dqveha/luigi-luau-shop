73a2a5c177232c650dbaeb9a7b784fa3bca099c4
Dave Lindqvist <Lindqvist.Dave@gmail.com>
2021-08-16 17:44:01 -0700

    Update landing page to have scopes

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,2 +1,13 @@
 class IndexController < ApplicationController
+
+  def index
+    @products = Product.all
+    @products_featured = Product.featured_products
+    @product_most_reviews = Product.most_reviews
+    @products_recent = Product.recent_three
+    @products_us = Product.made_in_us
+    render :index
+  end
+
 end
+
